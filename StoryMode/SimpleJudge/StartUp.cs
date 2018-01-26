f17c12b00794c115922e86516df088b03ff75d54
Shtereva <a.i.shtereva@gmail.com>
2018-01-26 22:12:24 +0200

    created judge system functionality

---
+++ StoryMode/SimpleJudge/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleJudge
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Tester.CompareContent(@"actualPath", @"expectedPath");
+        }
+    }
+}
