ae0f66fd8660cda209f1d4f3ce65b15be5aefd96
Shtereva <a.i.shtereva@gmail.com>
2018-01-26 19:11:13 +0200

    Added messages

---
+++ StoryMode/BashSoft/ExceptionMessages.cs
@@ -0,0 +1,16 @@
+﻿namespace BashSoft
+{
+    public static class ExceptionMessages
+    {
+        public static string DataAlreadyInitializedException = "Data is already initialized!";
+
+        public static string DataNotInitializedExceptionMessage =
+            "The data structure must be initialized first in order to make any operations with it.";
+
+        public static string InexistingCourseInDataBase =
+            "The course you are trying to get does not exist in the data base!";
+
+        public static string InexistingStudentInDataBase =
+            "The user name for the student you are trying to get does not exist!";
+    }
+}
